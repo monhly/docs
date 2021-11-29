7f260e3631270076ff9e37156e5ef2bdd67dc430
杨小兽 <yangxiaoshou@yangxiaoshoudeMacBook-Air.local>
2021-11-29 14:17:54 +0800

    feat:设置脚本

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,16 @@
 module.exports = {
+  base: "/docs/",
   title: "前端杨小兽",
   description: "自定义",
   head: [["link", { rel: "icon", href: "/favicon.ico" }]],
+  plugins: ["@vuepress/back-to-top"],
   themeConfig: {
     //导航栏的logo
     logo: "/assets/img/1.webp",
+
+    //页面滚动
+    smoothScroll: true,
+
     //多个下拉选项,
 
     nav: [
